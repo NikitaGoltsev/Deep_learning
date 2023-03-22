2b52e27b268adb072f23419c7c83eb889053a48f
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-03-22 13:22:40 +0700

    Repeat_sems_from_NSU

--- devivative.py
+++ devivative.py
@@ -1,1 +1,15 @@
 import numpy as np
+from sympy import *# That's lib created to work with devivvaitive
+
+class Dev_Functions_Set():
+    # I need to define most of functions here
+    def __init__(self) -> None:
+        
+        # You need to define x and any functions to calculete dvv from x
+        x = Symbol('x')
+        f = (x ** 2)/ (4 * x)
+        print(f.diff(x)) # dvv
+
+        return None
+
+obj1 = Dev_Functions_Set()
---
+++ sem_from_NSU/sem01.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from matplotlib import rc
+
+
+class Leniar_reg():
+    # I need to build it by the house dateset from kage
+    def __init__(self) -> None:
+         
+         return None
+        
+    
