e69feb54b4d34e5bcdb99da6c762e9df13a95b38
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-03-22 13:34:55 +0700

    Analyse_of_csv_with_train_date

--- sem_from_NSU/sem01.py
+++ sem_from_NSU/sem01.py
@@ -11,8 +11,11 @@ class Leniar_reg():
     # I need to build it by the house dateset from kage
     def __init__(self) -> None:
          
-        
+        self.df_train = pd.read_csv('house_prices_train.csv')
+        self.df_train['SalePrice'].describe()
         
         return None
         
-    
+
+obj1 = Leniar_reg()
+   
