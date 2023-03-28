1ccb3890402af931633fe26a3010ac71a08cddca
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-03-28 17:28:50 +0700

    tensor_circle

--- sem_from_NSU/tensir_example.py
+++ sem_from_NSU/tensir_example.py
@@ -12,11 +12,18 @@ class Tensor_Figurse():
         return None
     
     def circle(self, r:int = 5) -> torch.Tensor: # Create and retrunrn circle
-        # Cirle with r = 
+        # Cirle with r =
+        width = 5
         diametr = 2*r
         canvas = torch.zeros((diametr, diametr))# Torch tensor with in 2d space
-        # canvas - got only zero in every elem 
-        return None
+        # canvas - got only zero in every elem
+        # 
+        for i in range(diametr):
+            for j in range(diametr):
+                left_side = (i - r) ** 2 + (j - r) ** 2
+                if(left_side >= r**2 and left_side >= (r - width)**2):
+                    canvas[i,j] = 1
+        return canvas
 
     def square(self, x:int = 10) -> torch.Tensor:
 
@@ -25,3 +32,4 @@ class Tensor_Figurse():
     def block(self, x:int = 10, y:int = 10) -> torch.Tensor:
 
         return None
+    
