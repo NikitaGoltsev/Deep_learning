fb980b1f25ec6fa0da588121bcdf047e98ac58b3
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-03-28 17:24:22 +0700

    Try_to_work_with_Tensot

--- sem_from_NSU/sem01.py
+++ sem_from_NSU/sem01.py
@@ -10,12 +10,24 @@ from matplotlib import rc
 class Leniar_reg():
     # I need to build it by the house dateset from kage
     def __init__(self) -> None:
-         
-        self.df_train = pd.read_csv('house_prices_train.csv')
-        self.df_train['SalePrice'].describe()
         
+        self.df_train = pd.read_csv('house_prices_train.csv')
+        #print(self.df_train['SalePrice'].describe()) # some info
+        print(self.df_train.head())
+
+        self.__check_count_to_c__()
+
+
         return None
         
+    def __check_count_to_c__(self):
 
+        var = 'GrLivArea' # The 
+        data = pd.concat([self.df_train['SalePrice'], self.df_train[var]], axis=1)
+        data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000), s=32)
+        plt.show() # We can see linear 
+
+        return None
+    
+    
 obj1 = Leniar_reg()
-   
---
+++ sem_from_NSU/tensir_example.py
@@ -0,0 +1,27 @@
+import numpy as np
+from typing import Touple
+import torch
+
+# I need to work with tensor and img with torch fitches
+class Tensor_Figurse():
+
+    def __init__(self) -> None:
+        
+        #I'm going to init something if will need it
+
+        return None
+    
+    def circle(self, r:int = 5) -> torch.Tensor: # Create and retrunrn circle
+        # Cirle with r = 
+        diametr = 2*r
+        canvas = torch.zeros((diametr, diametr))# Torch tensor with in 2d space
+        # canvas - got only zero in every elem 
+        return None
+
+    def square(self, x:int = 10) -> torch.Tensor:
+
+        return None
+
+    def block(self, x:int = 10, y:int = 10) -> torch.Tensor:
+
+        return None
