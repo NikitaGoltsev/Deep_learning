0e21c0a817644581ee4ea0fdc7b53508796cc04b
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-04-07 00:30:08 +0700

    Date_work

--- Torch_tutr/work_with_date.py
+++ Torch_tutr/work_with_date.py
@@ -1,3 +1,20 @@
 import torch
+import torchvision
+from torch.utils.data import Dataset,DataLoader # Classes to work with data
 
 import numpy as np
+import math
+
+# In task we need to predict wine category
+class wine_dateset(Dataset):
+
+    def __init__(self) -> None:
+        # data loading
+    
+    def __getitem__(self, index):
+        #dataset[0]
+    
+    def __len__(self):
+
+        #get len of ctg
+
