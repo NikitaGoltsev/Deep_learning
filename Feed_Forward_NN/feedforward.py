962d1c927dec75f7153436451f27c3b5dd01607a
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-04-07 03:26:08 +0700

    Numbers_classification

--- Feed_Forward_NN/feedforward.py
+++ Feed_Forward_NN/feedforward.py
@@ -40,5 +40,72 @@ test_loader = torch.utils.data.DataLoader(dataset = train_dataset, batch_size = 
 
 examples = iter(train_loader)
 
-samples, labels = next(examples)
-print(samples.shape, labels.shape)
+# Size check
+#samples, labels = next(examples)
+#print(samples.shape, labels.shape)
+# output here is ----> 100, 1, 28, 28
+
+# How dose it looks ?
+#for i in range(6):
+    #plt.subplot(2, 3, i+1)
+    #plt.imshow(samples[i][0], cmap='gray')
+#plt.show()
+
+class NeuralNet(nn.Module):
+
+    def __init__(self, input_size, hiden_size, num_classes) -> None:
+        super(NeuralNet, self).__init__() # ?
+        # Creation of lyers
+        self.l1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.l2 = nn.Linear(hidden_size, num_class)
+    
+    def forward(self, x):
+        out = self.l1(x)
+        out = self.relu(out)
+        out = self.l2(out)
+        # No softmax because we are going to use crossentripy
+        return out
+
+model = NeuralNet(input_size, hidden_size, num_class)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
+
+# Training loop 
+n_total_srep = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader): # ?
+
+        # 100, 1, 28, 28
+        # 100, 784 --> We need to reshape out tenzor
+        images = images.reshape(-1, 28*28).to(device)
+        labels = labels.to(device)
+
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # Bachwards - ?
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if(i + 1) % 100 == 0:
+            print(f'epoch {epoch + 1}/{num_epochs}, step {i+1}/{n_total_srep}, loss = {loss.item():.4f}')
+
+# test
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    for images, labels in test_loader:
+        images = images.reshape(-1, 28*28).to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+
+        _, prediciton = torch.max(outputs, 1)
+        n_samples += labels.shape[0]
+        n_correct += (prediciton == labels).sum().item()
+
+    acc = 100.0 * n_correct/n_samples
+    
+    print(f'Final result is: accuracy = {acc}')
